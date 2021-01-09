c7bfbad77513c03a6992c226e7c1654db146eea4
İlker DOĞAN <ilkerdogan012327@gmail.com>
2021-01-09 16:04:51 +0300

    Müşteri eklemek için gereklli kodlar yazıldı ve arayüzünde değişiklik yapıldı.

--- Market/Form3.Designer.cs
+++ Market/Form3.Designer.cs
@@ -29,17 +29,18 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.IndianRed;
+            this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.button2);
@@ -47,8 +48,32 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(281, 450);
+            this.panel1.Size = new System.Drawing.Size(400, 450);
             this.panel1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 271);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(155, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Veresiye Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -60,38 +85,14 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(97, 329);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Tıkla";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(47, 81);
+            this.label1.Location = new System.Drawing.Point(116, 204);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(175, 40);
             this.label1.TabIndex = 3;
             this.label1.Text = "Müşteri Telefon No";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(86, 210);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(494, 134);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 238);
-            this.listBox1.TabIndex = 1;
             // 
             // Form3
             // 
@@ -99,10 +100,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SkyBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.panel1);
             this.Name = "Form3";
             this.Text = "Veresiye Satış İçin Müşteri Telefon No Girişi";
+            this.Load += new System.EventHandler(this.Form3_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -116,6 +117,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
--- Market/Form3.cs
+++ Market/Form3.cs
@@ -1,19 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Market
 {
     public partial class Form3 : Form
     {
-        public Form3()
+        private SatisEkrani frm1;
+        public Form3(SatisEkrani frm1)
         {
+            this.frm1 = frm1;
             InitializeComponent();
         }
 
@@ -22,6 +18,24 @@ namespace Market
             var m = new SatisEkrani();
             m.Show();
             this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string telNo = textBox1.Text;
+            using (var db = new DBModels())
+            {
+                var musteri = db.Musteriler.FirstOrDefault(t => t.MusteriTelNo == telNo);
+                if (musteri != null)
+                {
+                    musteri.Borc = Int32.Parse(frm1.textBox1.Text);
+                }
+            }
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = frm1.textBox1.Text;
         }
     }
 }
