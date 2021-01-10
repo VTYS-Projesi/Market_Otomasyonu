9b9e1ea662ef8d13f07d7124a3460271ef79627b
melike yıldırım <melikeyildirim718@gmail.com>
2021-01-10 18:26:09 +0300

    ürünlerimizin stoğa işlenmesi ve yeni ürün ekleme sayfasına yönlendirilme işleri arayüz kısmında yapıldı.

--- Market/Form7.Designer.cs
+++ Market/Form7.Designer.cs
@@ -32,6 +32,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,21 +59,36 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(306, 100);
+            this.button2.Location = new System.Drawing.Point(221, 65);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(212, 94);
+            this.button2.Size = new System.Drawing.Size(212, 110);
             this.button2.TabIndex = 1;
             this.button2.Text = "Stoğa İşle";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(471, 257);
+            this.button3.Location = new System.Drawing.Point(475, 65);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(178, 110);
             this.button3.TabIndex = 2;
             this.button3.Text = "txt Dosyasını Seç";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(233, 307);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Ürün Ekle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form7
             // 
@@ -79,6 +96,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SkyBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.panel1);
@@ -95,5 +113,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button4;
     }
 }
