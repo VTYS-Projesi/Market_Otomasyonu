b5dbcb03e9e4342883f168b9d62b96a6e2c8483f
melike yıldırım <melikeyildirim718@gmail.com>
2021-01-10 18:27:17 +0300

    yeni ürün eklemek için arayüz oluşturuldu.

---
+++ Market/UrunEkleme.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Market
+{
+    public partial class UrunEkleme : Form
+    {
+        public UrunEkleme()
+        {
+            InitializeComponent();
+        }
+
+        private void geri_Click(object sender, EventArgs e)
+        {
+            var m = new Form7();
+            m.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
