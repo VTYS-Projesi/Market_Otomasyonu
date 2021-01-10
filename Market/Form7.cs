a1121c89370be71fc460d1a834ea27ed7117901d
Metin Mert <mertinmerte@outlook.com>
2021-01-10 18:40:10 +0300

    Ürünlerimizin stoğa işlenmesinde gerekli olan .txt dosyasını seçme ve o dosya içeriğinin okunmasını sağlayan kod bloğu yazıldı.

--- Market/Form7.cs
+++ Market/Form7.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Market.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +29,30 @@ namespace Market
         private void button3_Click(object sender, EventArgs e)
         {
             openFileDialog1.ShowDialog();
+            string fileName = openFileDialog1.FileName;
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                StreamReader srd = new StreamReader(fs);
+                string ln;
+                while ((ln = srd.ReadLine()) != null)
+                {
+                    var urun = new Urunler();
+                    var values = ln.Split(',');
+                    urun.İrsaliyeNo = Int32.Parse(values[0]);
+                    urun.UrunKodu = Int32.Parse(values[1]);
+                    urun.BarkodNo = Int32.Parse(values[2]);
+                    urun.AlisFiyat = Int32.Parse(values[3]);
+                    urun.Miktar = Int32.Parse(values[4]);
+                    using (var db = new DBModels())
+                    {
+                        db.Urunler.Add(urun);
+                        db.SaveChanges();
+                    }
+                }
+                srd.Close();
+                fs.Close();
+
+            }
         }
 
         private void button4_Click(object sender, EventArgs e)
